29f9c58cad1198da00326eb34d920a02a5c600be
parrrate <alisa@parrrate.com>
2025-09-12 17:47:52 +0000

    simplify

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use futures_util::{Stream, TryStream, TryStreamExt};
 use genawaiter::sync::Gen;
 
-pub fn stream_batch_stream<K: Clone, V, S: TryStream<Ok = (K, V)>>(
+pub fn stream_batch_stream<K, V, S: TryStream<Ok = (K, V)>>(
     mut key: K,
-    mut next: impl FnMut(K) -> Result<K, S::Error>,
+    mut next: impl FnMut(&K) -> Result<K, S::Error>,
     mut fetch: impl FnMut(K) -> S,
 ) -> impl Stream<Item = Result<(K, V), S::Error>> {
     Gen::new(async move |co| {
@@ -13,7 +13,7 @@ pub fn stream_batch_stream<K: Clone, V, S: TryStream<Ok = (K, V)>>(
                 let Some((last, _)) = batch.last() else {
                     break Ok(());
                 };
-                key = next(last.clone())?;
+                key = next(last)?;
                 for item in batch {
                     co.yield_(Ok(item)).await;
                 }
