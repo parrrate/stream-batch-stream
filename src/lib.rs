2d6bafab5cea312905cd6aff39aae5b617dda2b5
parrrate <alisa@parrrate.com>
2025-09-12 17:47:52 +0000

    initial mess

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use futures_util::{Stream, TryStream, TryStreamExt};
+use genawaiter::sync::Gen;
+
+pub fn stream_batch_stream<K: Clone, V, S: TryStream<Ok = (K, V)>>(
+    mut key: K,
+    mut next: impl FnMut(K) -> Result<K, S::Error>,
+    mut fetch: impl FnMut(K) -> S,
+) -> impl Stream<Item = Result<(K, V), S::Error>> {
+    Gen::new(async move |co| {
+        let r: Result<(), S::Error> = async {
+            loop {
+                let batch = fetch(key).try_collect::<Vec<_>>().await?;
+                let Some((last, _)) = batch.last() else {
+                    break Ok(());
+                };
+                key = next(last.clone())?;
+                for item in batch {
+                    co.yield_(Ok(item)).await;
+                }
+            }
+        }
+        .await;
+        if let Err(e) = r {
+            co.yield_(Err(e)).await;
+        }
+    })
+}
